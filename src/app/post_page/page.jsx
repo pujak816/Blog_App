83b367a3de1d88e39b3d0efae753c3ed92e1f2ac
Puja <pujak816@gmail.com>
2023-10-05 19:13:41 +0530

    feat: built blog post page

--- src/app/post_page/page.jsx
+++ src/app/post_page/page.jsx
@@ -1,9 +1,15 @@
 import React from "react";
+import Link from "next/link";
 
 const BlogPagePost = () => {
   return (
-    <div className=" h-screen bg-black text-white">
-      <h1 className="text-2xl text-center py-36">Under construction</h1>
+    <div className="w-1/2 pt-32 mx-auto text-center">
+      <video src="https://cdnl.iconscout.com/lottie/premium/preview-watermark/under-construction-page-4474473-3718727.mp4?h=700"></video>
+      <Link href="/blog">
+        <button className="shadow-lg text-[10px] sm:text-lg font-semibold text-blue-600 px-4 py-1 hover:bg-blue-950 hover:text-white rounded-lg pt-2">
+          Check latest blogs
+        </button>
+      </Link>
     </div>
   );
 };
