388f33caf9efbb59851ba341f30ba50ce686a93a
Puja <pujak816@gmail.com>
2023-10-06 19:24:13 +0530

    style: change in css

---
+++ src/app/blog/page.js
@@ -0,0 +1,24 @@
+import React from "react";
+import getBlogs from "../../libs/getBlogs";
+import BlogCard from "../../components/BlogCard";
+
+const Blog = async ({}) => {
+  const data = await getBlogs();
+  const blogData = data.articles;
+
+  return (
+    <>
+      <div className="py-10 sm:w-2/3 lg:w-1/2 mx-auto pt-28 px-4">
+        <h1 className="text-2xl font-bold tracking-widest">BLOG</h1>
+        <p className="text-slate-500">Read our latest articles</p>
+      </div>
+      <section className="article_section flex flex-wrap justify-center ">
+        {blogData &&
+          Array.isArray(blogData) &&
+          blogData.map((item) => <BlogCard key={item.source.id} item={item} />)}
+      </section>
+    </>
+  );
+};
+
+export default Blog;
